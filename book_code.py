216acc63e0672d537c53c15fa6ae5b475da4d7f9
Andreas <aw6018a@student.american.edu>
2013-05-01 15:51:46 -0400

    supply and demand graphs

--- book_code.py
+++ book_code.py
@@ -121,24 +121,37 @@ def simulationProfit(P,t,r,M):
             data[i]=dP_dpi(data[i-1],t,r,M,i,fishCaughtPi(P,fish_caught[i-1],variable_cost,fixed_cost),variable_cost,fixed_cost) + data[i-1]
             fish_caught[i]=fishCaughtPi(P,fish_caught[i-1],variable_cost,fixed_cost)
     return data
-'''
-def demand(P,t,,r,M,quan,var,fixed):
+
+def demand(P,t,r,M):
     data=simulationProfit(P,t,r,M)
     demand=np.zeros_like(t)
     for i in range(len(t)):
-        fishCaughtPi(data[i],quan,var,fixed)
-'''
+        if i==0:
+            demand[i]=fishCaughtPi(data[i],0,variable_cost,fixed_cost)
+        else:
+            demand[i]=fishCaughtPi(data[i],demand[i-1],variable_cost,fixed_cost)
+    return demand
+
 
 sim_pi=simulationProfit(pop0, time_points,birth_frac,carry_capac)
 
+dem=demand(pop0,time_points,birth_frac,carry_capac)
+
 fish_pi_data=pd.DataFrame(np.column_stack((time_points,sim_pi)),columns=["Time (months)", "Fish"])
 print fish_pi_data
+demand_data=pd.DataFrame(np.column_stack((time_points,dem)),columns=["Time (months)", "Demand"])
+print demand_data
 
 
 fig, ax = plt.subplots(subplot_kw=dict(xlabel="Time (months)",ylabel="Fish Population"))
-p1=ax.plot(time_points,sim_pi, "k", lw=2)
-#p2=ax.plot(time_points,)
+ax.plot(time_points,sim_pi, "k", lw=2)
 plt.xlim(1,)
-fig.legend(p1,"S")
+fig.suptitle("Fish Population")
+plt.show()
+
+
+fig, ax = plt.subplots(subplot_kw=dict(xlabel="Time (months)",ylabel="Fish Demanded"))
+ax.plot(time_points,dem, "b", lw=2)
+plt.xlim(1,)
 fig.suptitle("Fish Population")
 plt.show()
