0567c211f65d0c7e52aabcefd4a277e3d979926d
Andreas <aw6018a@student.american.edu>
2013-05-01 14:59:37 -0400

    clean up, added profit dependent effort func

--- book_code.py
+++ book_code.py
@@ -4,17 +4,18 @@ import matplotlib.pyplot as plt
 from scipy import integrate
 import random
 
-#population = P
-#instantaneous growth rate = r
-#dP/dt = rP
-#initial pop = pop0
-#P=pop0 * e **(r*t)
-#deltapop = P(t)-P(t-dt) = (r*P(t-dt))*dt
-#carrying capacity = M
-#number of deaths = D
-#dD/dt = (r * (P/M))*P
-#dP/dt = (r*P) - (r * (P/M))*P = r*(1-(P/M))*P
-
+"""
+population = P
+instantaneous growth rate = r
+dP/dt = rP
+initial pop = pop0
+P=pop0 * e **(r*t)
+deltapop = P(t)-P(t-dt) = (r*P(t-dt))*dt
+carrying capacity = M
+number of deaths = D
+dD/dt = (r * (P/M))*P
+dP/dt = (r*P) - (r * (P/M))*P = r*(1-(P/M))*P
+"""
 
 def birthFunc(P, r):
     return (r * P)
@@ -22,46 +23,60 @@ def birthFunc(P, r):
 def deathFunc(P, t, r, M):
     return ((r*(P/M))*P)
 
-def priceFunc(q):
-    if ((10-.5*q)>1):
-        return (10-.5*q)
+def priceFunc(quan):
+    if ((20-.05*quan)>1):
+        return (20-.05*quan)
     else:
         return 1
 
-def costFunc(q):
-    return (100+10*q)
+def costFunc(quan,var,fixed): #q, 10, 100
+    return (fixed+var*quan)
 
-def revFunc(q):
-    return (priceFunc(q)*q)
+def revFunc(quan):
+    return (priceFunc(quan)*quan)
 
-def profitFunc(q):
-    return (revFunc(q)-costFunc(q)) 
+def profitFunc(quan, var, fixed):
+    return (revFunc(quan)-costFunc(quan,var,fixed)) 
 
-def unitProfit(q):
-    return (profitFunc(q)/q)
+def unitProfit(quan):
+    return (profitFunc(quan)/quan)
 
-def MR(q):
-    return (10-q)
+def MR(quan):
+    return (20-quan)
 
-def MC(q):
+def MC(quan):
     return 10
-
-#E(t)=.1*t
-#E'(t) = .1
-def effortFunc(t):
-    #why does the np.random raise an error?
-    #replaced with python random, slower I think.
-    prob_catch = np.random.random()
-    return (prob_catch/(t+1))
 
 def dEffort_dt(t):
     #print "t ",t
     return np.random.random()
 
+#E(t)=.1*t
+#E'(t) = .1
+def effortFunc(t):
+    prob_catch = np.random.random()
+    return (prob_catch/(t+1))
+
+def effortProfit(quan, var, fixed):
+    prob_catch = np.random.random()
+    return (profitFunc(quan,var,fixed)*prob_catch)
+
 def fishCaught(P,t,iteration):
+    """
+    fish caught with respect to time
+    """
     return (effortFunc(t[iteration])*P)
 
+def fishCaughPi(P,quan,var,fixed): 
+    """
+    fish caught with respect to profit
+    """
+    return (effortProfit(quan,var,fixed)*P)
+
 def dP_dt(P,t,r,M,iteration):
+    return (birthFunc(P,r)-deathFunc(P,t,r,M)-fishCaught(P,t,iteration))
+
+def dP_dpi(P,t,r,M,iteration,quan,var,fixed):
     return (birthFunc(P,r)-deathFunc(P,t,r,M)-fishCaught(P,t,iteration))
 
 pop0 = 1000
@@ -72,24 +87,31 @@ carry_capac = 2000
 time_points = np.arange(1,49,1.)
 print time_points
 
-def simulation(P,t,r,M):
+def simulationTime(P,t,r,M): #effort with respect to time
     data=np.zeros_like(t)
     for i in range(len(t)-1):
         if i==0:
             data[i]=dP_dt(P,t,r,M,i) + P
         else:
-            data[i]=dP_dt(data[i-1],t,r,M,i)+data[i-1] #this is the change in pop. not total pop
+            data[i]=dP_dt(data[i-1],t,r,M,i)+data[i-1]
     return data
 
-sim=simulation(pop0, time_points, birth_frac, carry_capac)
+sim_time=simulationTime(pop0, time_points, birth_frac, carry_capac)
 #pops = integrate.odeint(dP_dt, pop0, time_points, args=(birth_frac, carry_capac))
 
-fish_data=pd.DataFrame(np.column_stack((time_points,sim)),columns=["Time (months)", "Fish"])
+fish_data=pd.DataFrame(np.column_stack((time_points,sim_time)),columns=["Time (months)", "Fish"])
 print fish_data
 
 fig, ax = plt.subplots(subplot_kw=dict(xlabel="Time (months)",ylabel="Fish Population"))
-ax.plot(time_points,sim, "k", lw=2)
+ax.plot(time_points,sim_time, "k", lw=2)
 #ax.plot(time_points,)
 plt.xlim(1,)
 fig.suptitle("Fish Population")
 plt.show()
+
+def simulationProfit(P,t,r,M):
+    data=np.zeros_like(t)
+    for i in range(len(t)):
+        data[i]=func 
+    return data
+
